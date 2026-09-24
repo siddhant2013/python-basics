f461d451974000b6a90ed6fd4e4beea2cbc5d88c
siddhant2013 <siddhant.rout8@gmail.com>
2026-09-24 08:07:55 -0400

    Introduction to Loops

---
+++ starpattern.py
@@ -0,0 +1,5 @@
+num = int(input("enter the no . of rows"))
+for i in range(1, num+1):
+    for j in range(i):
+        print("*", end = " ")
+    print()
---
+++ sum.py
@@ -0,0 +1,8 @@
+num = 1
+sum = 0
+
+while num <=10:
+    sum = sum + num
+    num = num+1
+
+print("the sum of the first natural num is ", sum)
